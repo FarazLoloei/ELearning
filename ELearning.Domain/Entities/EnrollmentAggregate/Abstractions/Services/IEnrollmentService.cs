9f405ff08e58d98b30742547426c9b0c5aa511e3
Faraz Loloei <farazloloei@uni-koblenz.de>
2025-03-10 00:33:07 +0100

    Entities Services Abstraction is added

---
+++ ELearning.Domain/Entities/EnrollmentAggregate/Abstractions/Services/IEnrollmentService.cs
@@ -0,0 +1,14 @@
+﻿namespace ELearning.Domain.Entities.EnrollmentAggregate.Abstractions.Services;
+
+public interface IEnrollmentService
+{
+    Task<bool> CanStudentEnrollAsync(Guid studentId, Guid courseId);
+
+    Task<bool> HasStudentCompletedPrerequisitesAsync(Guid studentId, Guid courseId);
+
+    Task<double> CalculateCourseCompletionPercentageAsync(Guid enrollmentId);
+
+    Task<bool> CheckIfCourseCompletedAsync(Enrollment enrollment);
+
+    Task ProcessEnrollmentCompletionAsync(Enrollment enrollment);
+}
---
+++ ELearning.Domain/Entities/EnrollmentAggregate/Abstractions/Services/ISubmissionService.cs
@@ -0,0 +1,8 @@
+﻿namespace ELearning.Domain.Entities.EnrollmentAggregate.Abstractions.Services;
+
+public interface ISubmissionService
+{
+    Task<bool> CanGradeSubmissionAsync(Guid instructorId, Guid submissionId);
+
+    Task NotifyStudentOfGradedSubmissionAsync(Submission submission);
+}
